c73f2895fc2562423acce18bb2e8033e6492fa87
Marco H <marco.hdez0928@gmail.com>
2021-01-05 23:39:44 -0600

    se agrega zeromatrix

--- src/CrackingCodingInterview/MyCoding/ArraysAndStrings/zeroMatrix.java
+++ src/CrackingCodingInterview/MyCoding/ArraysAndStrings/zeroMatrix.java
@@ -5,10 +5,39 @@
  */
 package CrackingCodingInterview.MyCoding.ArraysAndStrings;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author MarcoHdez
  */
 public class zeroMatrix {
+    
+    public int[][] setZeroes(int[][] matrix){
+        LinkedList<Integer> x=new LinkedList();
+        LinkedList<Integer> y=new LinkedList();    
+        //save positions in X and Y with elements equals to zero
+        for(int j=0;j<matrix.length; j++){
+            for(int i=0;i<matrix[0].length;i++){
+                if(matrix[j][i]==0){
+                    x.add(j);
+                    y.add(i);
+                }
+            }
+        }
+        //replace selected elements in positions in x with zero
+        for(int i=0;i<x.size();i++){
+            for(int j=0;j<matrix.length;j++){
+                matrix[x.get(i)][j]=0;
+            }
+        }
+        for(int i=0;i<y.size();i++){
+            for(int j=0;j< matrix[0].length;j++){
+                matrix[j][y.get(i)]=0;
+            }
+        }
+        return matrix;
+    }
+    
     
 }
---
+++ src/CrackingCodingInterview/MyTest/ArraysAndStrings/TestZeroMatrix.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CrackingCodingInterview.MyTest.ArraysAndStrings;
+import CrackingCodingInterview.MyCoding.ArraysAndStrings.zeroMatrix;
+//import CrackingCodingInterview.MyTest.Generate.
+/**
+ *
+ * @author MarcoHdez
+ */
+public class TestZeroMatrix {
+    public static void main(String args[]){
+        
+    }
+}
